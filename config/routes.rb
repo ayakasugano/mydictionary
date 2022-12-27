e976b7e41f2b18c5338f7177ad54df7fcaea2470
ayakasugano <suganoayaka@kannoayakanoMacBook-Pro.local>
2022-12-27 16:58:44 +0900

    bin/rails g controller words index show new edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'words/index'
+  get 'words/show'
+  get 'words/new'
+  get 'words/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
