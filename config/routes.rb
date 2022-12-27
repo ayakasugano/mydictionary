0e0d33aba83b3837115172cd3002b415c89630ab
ayakasugano <suganoayaka@kannoayakanoMacBook-Pro.local>
2022-12-27 17:00:32 +0900

    ルーティングを一括で設定した

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'words/index'
-  get 'words/show'
-  get 'words/new'
-  get 'words/edit'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'words#index'
+  resources :words
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
