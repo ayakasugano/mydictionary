d798b303d02daf6e375acf968a8fc1944c4dc95a
ayakasugano <suganoayaka@kannoayakanoMacBook-Pro.local>
2022-12-28 16:19:24 +0900

    編集画面を作成し、新規登録画面のフォームと共通化した

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,6 @@
 class WordsController < ApplicationController
   def index
+    @words = Word.all
   end
 
   def show
@@ -9,6 +10,26 @@ class WordsController < ApplicationController
     @word = Word.new
   end
 
-  def edit
+  def create
+    word = Word.new(word_params)
+    word.save!
+    redirect_to words_url, notice: "#{word.name}を登録しました。"
   end
+
+  def edit
+    @word = Word.find(params[:id])
+  end
+
+  def update
+    word = Word.find(params[:id])
+    word.update!(word_params)
+    redirect_to words_url, notice: "#{word.name}を更新しました。"
+  end
+
+  private
+
+  def word_params
+    params.require(:word).permit(:name, :word_class, :meaning)
+  end
+
 end
